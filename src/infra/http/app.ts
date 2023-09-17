4d34c4fc666fb778cbffcacbb903bdbe8b59e699
Silas Stoffel <silasstofel@gmail.com>
2023-09-17 15:23:17 -0300

    doc: open-api + swagger

--- src/infra/http/app.ts
+++ src/infra/http/app.ts
@@ -1,16 +1,20 @@
 import 'reflect-metadata';
 import express from 'express';
+import swaggerUi from "swagger-ui-express";
 import * as dotenv from 'dotenv';
 dotenv.config();
+
 
 import './../database/mongoose/config';
 import '../providers/ioc'
 
 import { router } from "./routes";
+import swaggerFile from '../../swagger.json';
 
 const app = express();
 
 app.use(express.json());
+app.use('/api-reference', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
 
 export { app };
